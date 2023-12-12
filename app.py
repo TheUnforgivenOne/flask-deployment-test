89650a48b0f329df7e25ca81417f1afef62909fa
TheUnforgivenOne <vladpotapow@yandex.ru>
2023-12-12 20:49:17 +0100

    Added sample env value

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def sample_route():
-  return '<h1>Hello world!</h1>'
+  env_value = os.environ.get('ENV_KEY')
+  return f'<h1>Hello world! {env_value}</h1>'
 
 
 if __name__ == "__main__":
